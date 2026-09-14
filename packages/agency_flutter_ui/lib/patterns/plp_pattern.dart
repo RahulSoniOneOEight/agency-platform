d8ccf6cd4e900b0cdf34629a2bfa8befd53c7369
RahulSoniOneOEight <rs.jgd108@gmail.com>
2026-09-14 14:12:25 +0530

    feat: add PLP pattern shell

---
+++ packages/agency_flutter_ui/lib/patterns/plp_pattern.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import '../domain/product_card.dart';
+import '../domain/product_models.dart';
+import '../foundation/agency_tokens.dart';
+import '../primitives/agency_chip.dart';
+import 'pattern_shell.dart';
+
+class PlpPattern extends StatelessWidget {
+  const PlpPattern({
+    super.key,
+    required this.products,
+    this.title = 'Products',
+    this.density = AgencyDensity.balanced,
+    this.b2b = false,
+  });
+
+  final List<AgencyProduct> products;
+  final String title;
+  final AgencyDensity density;
+  final bool b2b;
+
+  @override
+  Widget build(BuildContext context) {
+    return AgencyPatternShell(
+      title: title,
+      density: density,
+      children: [
+        const Wrap(spacing: 8, children: [
+          AgencyChip(label: 'In stock'),
+          AgencyChip(label: 'Popular'),
+          AgencyChip(label: 'Price'),
+        ]),
+        GridView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: products.length,
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 280,
+            mainAxisExtent: 290,
+            crossAxisSpacing: 12,
+            mainAxisSpacing: 12,
+          ),
+          itemBuilder: (context, index) => ProductCard(
+            product: products[index],
+            variant: b2b ? ProductCardVariant.b2b : ProductCardVariant.standard,
+            density: density,
+          ),
+        ),
+      ],
+    );
+  }
+}
